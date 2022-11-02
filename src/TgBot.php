a9c5ef77f5f1f21e020f8a1080113129b0948eac
YuriyMironov <jura120596@gmail.com>
2022-11-02 12:04:11 +0300

    start command code

--- src/TgBot.php
+++ src/TgBot.php
@@ -13,7 +13,7 @@ use TgBotApi\BotApiBase\Method\SetWebhookMethod;
 use TgBotApi\BotApiBase\Type\InlineKeyboardMarkupType;
 use TgBotApi\BotApiBase\Type\MessageType;
 use TgBotApi\BotApiBase\Type\ReplyKeyboardMarkupType;
-use App\Http\Requests\Bot\BotCallbackRequest;
+use Yumir\TgBotLaravel\Requests\BaseBotCallbackRequest;
 
 class TgBot extends BotApiComplete
 {
@@ -21,19 +21,19 @@ class TgBot extends BotApiComplete
     {
         \Facades\Yumir\TgBotLaravel\TgBot::set(SetWebhookMethod::create(config('tgbot.whurl') . route('botcmd', [], false)));
         Handler::defineChatCommands();
-        \Facades\Yumir\TgBotLaravel\TgBot::send(\TgBotApi\BotApiBase\Method\SendMessageMethod::create(config('tgbot.dev_id'), 'Config updated'));
+        \Facades\Yumir\TgBotLaravel\TgBot::send(SendMessageMethod::create(config('tgbot.dev_id'), 'Config updated'));
     }
     /**
-     * @param BotCallbackRequest $r
+     * @param BaseBotCallbackRequest $r
      * @param string $message
      * @param array|null $data
      */
-    public static function write(BotCallbackRequest $r, string $message, array $data = null): MessageType
+    public static function write(BaseBotCallbackRequest $r, string $message, array $data = null): MessageType
     {
         return \Facades\Yumir\TgBotLaravel\TgBot::sendMessage(SendMessageMethod::create($r->getUser()->tg_id ?: $r->getChat()['id'], $message, $data));
     }
 
-    public static function changeMessageMarkup(BotCallbackRequest $r, array $keyboard, bool $inline = true)
+    public static function changeMessageMarkup(BaseBotCallbackRequest $r, array $keyboard, bool $inline = true)
     {
         if ($r->isCallback() && $r->getMessage()) {
             return \Facades\Yumir\TgBotLaravel\TgBot::editMessageReplyMarkup(EditMessageReplyMarkupMethod::create($r->getChat()['id'], $r->getMessage()['message_id'], [
@@ -42,12 +42,12 @@ class TgBot extends BotApiComplete
         }
     }
 
-    public static function deleteMsg(BotCallbackRequest $request)
+    public static function deleteMsg(BaseBotCallbackRequest $request)
     {
         \Facades\Yumir\TgBotLaravel\TgBot::delete(DeleteMessageMethod::create($request->getChat()['id'], $request->getMessage()['message_id']));
     }
 
-    public static function sendInvoiceMessage(BotCallbackRequest $request,
+    public static function sendInvoiceMessage(BaseBotCallbackRequest $request,
                                               $title,
                                               $description,
                                               $payload,
